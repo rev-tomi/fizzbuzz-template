ec22df8c68ab9b38608a26b392fdecfda0900b36
Tamas Rev <extern.trev@porsche.hu>
2015-08-03 22:48:33 +0200

    refactoring tests

--- src/test/java/com/trev/fizzbuzz/template/TestFizzBuzzText.java
+++ src/test/java/com/trev/fizzbuzz/template/TestFizzBuzzText.java
@@ -2,22 +2,37 @@ package com.trev.fizzbuzz.template;
 
 import static org.testng.Assert.assertEquals;
 
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestFizzBuzzText
 {
 
-    @Test
-    public void testFizzBuzzText()
-    {
-        // GIVEN
-        final FizzBuzzTemplate sut = new FizzBuzzTemplate();
+    private FizzBuzzTemplate sut;
 
+    @BeforeMethod
+    void setUp()
+    {
+        sut = new FizzBuzzTemplate();
+    }
+
+    @Test(dataProvider = "fizzBuzzText")
+    public void testFizzBuzzText(final Integer num, final String expected)
+    {
         // WHEN
-        final String text = sut.getFizzBuzzText(1);
+        final String text = sut.getFizzBuzzText(num);
 
         // THEN
-        assertEquals("1", text);
+        assertEquals(text, expected);
+    }
+
+    @DataProvider(name = "fizzBuzzText")
+    private Object[][] getFizzBuzzTextTestData()
+    {
+        return new Object[][]{
+            new Object[]{1, "1"},
+        };
     }
 
 }
