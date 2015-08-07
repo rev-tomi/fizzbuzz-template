f7c691f3441853addc1b57e5a3e8198114bdca96
Tamas Rev <extern.trev@porsche.hu>
2015-08-07 10:25:26 +0200

    adding a main

---
+++ src/main/java/com/trev/fizzbuzz/template/main/Main.java
@@ -0,0 +1,14 @@
+package com.trev.fizzbuzz.template.main;
+
+import com.trev.fizzbuzz.template.FizzBuzzTemplate;
+
+public class Main
+{
+
+    public static void main(final String[] args)
+    {
+        final FizzBuzzTemplate fizzBuzz = new FizzBuzzTemplate();
+        fizzBuzz.doFizzBuzz();
+    }
+
+}
